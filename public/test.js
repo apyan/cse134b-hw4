08d84b9849d26396441002503ce9cd82ed8a9a93
Mark Santa Presca <marksantapresca@gmail.com>
2017-03-01 22:53:50 -0800

    put in the new user info

--- public/test.js
+++ public/test.js
@@ -5,11 +5,46 @@ function submitClick() {
 	window.alert("bye");
 }
 
-function writeUserData(userId, password) {
-  window.alert(userId + password);
+function writeNewUserData(userId, password) {
+  window.alert(userId + " " + password);
   firebase.database().ref().child(userId).set({
     username: userId,
-    password: password
+    password: password,
+    originalList: {
+      mario: false, 
+      peach: false,
+      yoshi: false,
+      donkeykong: false,
+      link: false,
+      fox: false,
+      samus: false,
+      wiifittrainier: false,
+      villager: false,
+      pikachu: false,
+      kirby: false,
+      marth: false,
+      zelda: false,
+      luigi: false,
+      littlemac: false,
+      pit: false,
+      captainfalcon: false,
+      rosalinaandluma: false,
+      bowser: false,
+      lucario: false,
+      toonlink: false,
+      sheik: false,
+      ike: false,
+      shulk: false,
+      sonic: false,
+      megaman: false,
+      kingdedede: false,
+      metaknight: false,
+      robin: false,
+      lucina: false,
+      wario: false,
+      charizard: false,
+      ness: false    }
   });
+
   window.alert("did something");
 }

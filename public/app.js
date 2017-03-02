e84062518d132ebb8bc48cd27c21258f35a44317
Alvin Yan <apyan@ucsd.edu>
2017-03-02 05:34:13 -0800

    Sign Up (Public) Updated
    So far, users can signup with their email address and password. This
    seems like to only route I've found for accounts.
    View users by Authentication -> Users tab

--- public/app.js
+++ public/app.js
@@ -12,3 +12,13 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
+// Sign In Functions
+AmiiboDex.prototype.initFirebase = function() {
+  // Shortcuts to Firebase SDK features.
+  this.auth = firebase.auth();
+  this.database = firebase.database();
+  this.storage = firebase.storage();
+  // Initiates Firebase auth and listen to auth state changes.
+  this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
+};
+

75f367657bd110deced5177c7c110a1bc855e09e
Tony Ly <t2ly@ucsd.edu>
2017-03-02 20:25:55 -0800

    Added reading from database for public/index.html

--- public/test.js
+++ public/test.js
@@ -1,10 +1,3 @@
-function submitClick() {
-	window.alert("hi");
-	var firebaseRef = firebase.database().ref();
-	firebaseRef.child("Text").set("Some Value");
-	window.alert("bye");
-}
-
 function writeNewUserData(userId, password) {
   window.alert(userId + " " + password);
   firebase.database().ref().child(userId).set({
@@ -48,3 +41,64 @@ function writeNewUserData(userId, password) {
 
   window.alert("did something");
 }
+
+
+var firebaseAmiiboListRef = firebase.database().ref().child("original-amiibo-list");
+demoP = document.getElementById("demo");
+var characters = ["SkipThis","Mario", "Peach", "Yoshi", "DonkeyKong", "Link", "Fox", "Samus", "WiiFitTrainer", "Villager", "Pikachu", "Kirby", "Marth", "Zelda", "DiddyKong", "Luigi", "LittleMac", "Pit", "CaptainFalcon", "Rosalina&Luma", "Bowser", "Lucario", "ToonLink", "Sheik", "Ike", "Shulk", "Sonic", "MegaMan", "KingDedede", "MetaKnight", "Robin", "Lucina", "Wario", "Charizard", "Ness"];
+
+function forEachFunction(item, index) {
+  // skip 0th element
+  if(index == 0){
+    //alert("skipped");
+    return;
+  }
+  var number = document.getElementById(item + "-Number");
+  var firebaseCharacterRef = firebaseAmiiboListRef.child(index + "-" + item);
+  var firebaseCharacterNumberRef = firebaseCharacterRef.child("Number");
+  firebaseCharacterNumberRef.on('value', function(snapshot){
+    number.innerHTML = snapshot.val();
+  });
+  var name = document.getElementById(item + "-Name");
+  var firebaseCharacterNameRef = firebaseCharacterRef.child("Name");
+  firebaseCharacterNameRef.on('value', function(snapshot){
+    name.innerHTML = snapshot.val();
+  });
+  var image = document.getElementById(item + "-Image");
+  var firebaseCharacterImageRef = firebaseCharacterRef.child("Image");
+  firebaseCharacterImageRef.on('value', function(snapshot){
+    image.src = snapshot.val();
+  });
+  var gameOrigin = document.getElementById(item + "-GameOrigin");
+  var firebaseCharacterGameOriginRef = firebaseCharacterRef.child("Game Origin");
+  firebaseCharacterGameOriginRef.on('value', function(snapshot){
+    gameOrigin.innerHTML = snapshot.val();
+  });
+  var dateRelease = document.getElementById(item + "-DateRelease");
+  var firebaseCharacterDateReleaseRef = firebaseCharacterRef.child("Date Release");
+  firebaseCharacterDateReleaseRef.on('value', function(snapshot){
+    dateRelease.innerHTML = snapshot.val();
+  });
+  var wave = document.getElementById(item + "-Wave");
+  var firebaseCharacterWaveRef = firebaseCharacterRef.child("Wave");
+  firebaseCharacterWaveRef.on('value', function(snapshot){
+    wave.innerHTML = snapshot.val();
+  });
+  var exclusive = document.getElementById(item + "-Exclusive");
+  var firebaseCharacterExclusiveRef = firebaseCharacterRef.child("Exclusive");
+  firebaseCharacterExclusiveRef.on('value', function(snapshot){
+    exclusive.innerHTML = snapshot.val();
+  });
+  var description = document.getElementById(item + "-Description");
+  var firebaseCharacterDescriptionRef = firebaseCharacterRef.child("Description");
+  firebaseCharacterDescriptionRef.on('value', function(snapshot){
+    description.innerHTML = snapshot.val();
+  });
+  var rarity = document.getElementById(item + "-Rarity");
+  var firebaseCharacterRarityRef = firebaseCharacterRef.child("Rarity");
+  firebaseCharacterRarityRef.on('value', function(snapshot){
+    rarity.innerHTML = snapshot.val();
+  });
+}
+
+characters.forEach(forEachFunction);

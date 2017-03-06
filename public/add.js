93341aa2b819a30ce93ea923dbb0b879f97b2264
Tony Ly <t2ly@ucsd.edu>
2017-03-06 13:38:37 -0800

    implemented picture adding and editing

--- public/add.js
+++ public/add.js
@@ -1,27 +1,85 @@
-function addAmiibo(userId) {
-	var number = document.getElementById("number").value;
-	var name = document.getElementById("name").value;
-	var origin = document.getElementById("origin").value;
-	var date = document.getElementById("date").value;
-	var wave = document.getElementById("wave").value;
-	var exclusive = document.getElementById("exclusive").value;
-	var description = document.getElementById("description").value;
-	var rarity = document.getElementById("rarity").value;
-	var have = document.getElementById("have").checked;
+var uploaded = 0;
+var backToIndex = 0;
+var pictureCustomRef;
+var downloadURL;
+var number;
+var name;
+var origin;
+var date;
+var wave;
+var exclusive;
+var description;
+var rarity;
+var have;
 
-	// change customList1 to customList
-	firebase.database().ref().child("users/" + userId + "/customList/" + number + "-" + name).set({
-		DateRelease: date,
-		Description: description,
-		Exclusive: exclusive,
+function waitReturnToIndex(){
+	if(backToIndex != 1) {//we want it to match
+        alert("waiting");
+        setTimeout(waitReturnToIndex, 50);//wait 50 millisecnds then recheck
+        return;
+    }
+    window.location.href = "indexsignedin.html";
+}
+function waitForURL() {
+    if(downloadURL===null) {//we want it to match
+        setTimeout(waitForURL, 50);//wait 50 millisecnds then recheck
+        return;
+    }
+    firebase.database().ref().child("users/" + authId + "/customList/" + number + "-" + name).set({
+        DateRelease: date,
+        Description: description,
+        Exclusive: exclusive,
         GameOrigin: origin,
-        Image: "https://firebasestorage.googleapis.com/v0/b/amiibodex.appspot.com/o/Original-Amiibo%2F11_kirby.jpg?alt=media&token=b1dfb09b-8185-4d9a-9d23-ab04e7e89cd4",
+        Image: downloadURL,
         Name: name,
-       	Number: number,
-       	Rarity: rarity,
-       	Wave: wave,
+        Number: number,
+        Rarity: rarity,
+        Wave: wave,
         Have: have
-	});
-	alert("added");
-	//window.location.href = "indexsignedin.html";
+    }).then(function(url){
+        backToIndex = 1;
+        waitReturnToIndex();
+    });
+}
+function waitToUpload() {
+    if(uploaded!=1) {//we want it to match
+        setTimeout(waitToUpload, 50);//wait 50 millisecnds then recheck
+        return;
+    }
+    pictureCustomRef.getDownloadURL().then(function(url){
+    	downloadURL = url;
+    });
+    waitForURL();
+}
+
+function addAmiibo(authId) {
+	var file;
+	var image = document.getElementById("image");
+    if ("files" in image) {
+    	file = image.files[0];
+    } 
+
+    var pictureRef = customStorageRef.child(file.name);
+    pictureCustomRef = customStorageRef.child("Custom-Amiibo/" + authId + "/" + file.name);
+
+    pictureRef.name === pictureCustomRef.name;
+    pictureRef.fullPath === pictureCustomRef.fullPath;
+   	uploaded = 0;
+    pictureCustomRef.put(file).then(function(snapshot){
+    	uploaded = 1;
+    });
+
+    downloadURL = null;
+    waitToUpload();
+    
+	number = document.getElementById("number").value;
+	name = document.getElementById("name").value;
+	origin = document.getElementById("origin").value;
+	date = document.getElementById("date").value;
+	wave = document.getElementById("wave").value;
+	exclusive = document.getElementById("exclusive").value;
+	description = document.getElementById("description").value;
+	rarity = document.getElementById("rarity").value;
+	have = document.getElementById("have").checked;
+
 }
